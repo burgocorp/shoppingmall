082aa2e76a8e89dc7ba9049c77c80b71f404ca74
Chung <burgocorp@gmail.com>
2019-11-16 17:18:10 +0900

    router #2 and CRUD

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,12 +6,33 @@ const router = express.Router();
 
 // 오더와 관련된 api 즉 order CRUD는 여기서 진행될 것이다 
 
+//오더데이터 불러오기 
 router.get('/', (req, res) => {
     res.json({
         msg: "orderdate get"
     });
 });
 
+//오더 데이터 생성하기 
+router.post('/', (req, res) => {
+    res.json({
+        msg: "데이터 생성됨"
+    });
+});
+
+//오더 데이터 수정하기 
+router.patch('/', (req, res) => {
+    res.json({
+        msg: "데이터 수정하기"
+    });
+});
+
+//오더 데이터 삭제하기 
+router.delete('/', (req, res) => {
+    res.json({
+        msg: "데이터 삭제하기"
+    });
+});
 
 
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -2,9 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 // 프로덕트와 관련된 api 즉 데이타CRUD 가 여기서 진행 될 것이다 
+
+// products데이타 불러오기 
 router.get('/', (req, res) => {
     res.json({
         msg: "productdata get"
+    });
+});
+
+// product 데이터 생성하기 
+router.post('/', (req, res) =>{
+    res.json({
+        msg: "데이터 생성됨"
+    });
+});
+
+// products 데이터 수정하기 
+router.patch('/', (req, res) => {
+    res.json({
+        msg: "데이터 수정됨"
+    });
+});
+
+// products 데이터 삭제하기 
+router.delete('/', (req, res) =>{
+    res.json({
+        msg: "데이터 삭제하기"
     });
 });
 
