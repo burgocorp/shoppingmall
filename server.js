4676bf43b82584929c33b9028ac21f5fabefe44f
Chung <burgocorp@gmail.com>
2019-11-16 17:50:54 +0900

    add body-parser

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 //몽구스라이브러리를 불러온다
 const mongoose = require('mongoose'); 
+//morgan을 불러온다 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 
 
 // server.js 파일에서  라우트를 만들어서 상수화 시켜준다 
@@ -10,12 +14,18 @@ const mongoose = require('mongoose');
 const productsRoute = require('./api/routes/products');
 const ordersRoute = require('./api/routes/orders');
 
-//몽고디비 유알엘을 넣어준다 
+//몽고디비연결 코드 , 유알엘을 넣어준다 
 const mongoDBurl = "mongodb+srv://myounghwan:qnfmrh0228@cluster0-1dywn.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoDBurl, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("MongoDB connected..."))
     .catch(err => console.log(err.message));
 
+
+    //다양한 버젼을 테스트 해볼것
+app.use(morgan("dev"));
+//body-parser 추가 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
 
 
 app.use('/products', productsRoute);
