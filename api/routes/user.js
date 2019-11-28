05ebd0e3a9ca99af60ca0a532ddd028337111621
Chung <burgocorp@gmail.com>
2019-11-28 12:00:54 +0900

    add bcryptjs to password

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,31 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
+ //password 암호화 library
+const bcrypt = require('bcryptjs');
 const userModel = require('../models/user');
 
 
 // 회원가입
 router.post('/register', (req, res) => {
-    const user = new userModel({
-        name : req.body.username,
-        email : req.body.email,
-        password : req.body.password
-    });
+    
 
-    user
-        .save()
-        .then(result => {
-            res.json({
-                msg : "registered user",
-                userInfo : result
-            });
-           
-        })
-        .catch(err => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
             res.json({
                 msg : err.message
             });
-        });
+        } else {
+            const user = new userModel({
+                name : req.body.username,
+                email : req.body.email,
+                password : hash
+            });
+        
+    
+            user
+                .save()
+                .then(result => {
+                    res.json({
+                        msg : "registered user",
+                        userInfo : result
+                    });
+                   
+                })
+                .catch(err => {
+                    res.json({
+                        msg : err.message
+                    });
+                });
+
+        }
+    })
+
+
+   
 
 });
 
